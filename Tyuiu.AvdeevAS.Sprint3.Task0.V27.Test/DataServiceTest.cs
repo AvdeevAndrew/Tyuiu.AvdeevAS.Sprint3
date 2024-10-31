ae1af37220cede6904176d62e7b273bb57204787
Tyuiu.AvdeevAS <Tyuiu.AvdeevAS@outlook.com>
2024-10-31 20:11:15 +0500

    Add project files.

---
+++ Tyuiu.AvdeevAS.Sprint3.Task0.V27.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Tyuiu.AvdeevAS.Sprint3.Task0.V27.Lib;
+
+namespace Tyuiu.AvdeevAS.Sprint3.Task0.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double value = 5;
+            int startValue = 1;
+            int stopValue = 13;
+
+            // Act
+            double result = ds.GetSumSeries(value, startValue, stopValue);
+
+            // Assert
+            Assert.AreEqual(16.016, Math.Round(result,3));
+        }
+    }
+}
