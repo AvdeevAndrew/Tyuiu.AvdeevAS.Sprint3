a6404fe7cd134bceced4b1e8e318bd0edbd3be74
Tyuiu.AvdeevAS <Tyuiu.AvdeevAS@outlook.com>
2024-10-31 20:13:48 +0500

    fix

--- Tyuiu.AvdeevAS.Sprint3.Task0.V27.Lib/DataService.cs
+++ Tyuiu.AvdeevAS.Sprint3.Task0.V27.Lib/DataService.cs
@@ -1,14 +1,15 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.AvdeevAS.Sprint3.Task0.V27.Lib
 {
-    public class DataService : ISprint3Task0V0
+    public class DataService : ISprint3Task0V27
     {
-        public double GetSumSeries(double value, int startValue, int stopValue)
+        
+        public double GetSumSeries(int value, int startValue, int stopValue)
         {
             double sum = 0;
             for (int k = startValue; k <= stopValue; k++)
             {
-                
+
                 sum += Math.Pow(4.0 / Math.Pow(k, value), 2);
             }
             return sum;
