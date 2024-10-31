4923218ac512d617e12fbad6b35a1b5297bb9848
Tyuiu.AvdeevAS <Tyuiu.AvdeevAS@outlook.com>
2024-10-31 21:31:03 +0500

    Task3

---
+++ Tyuiu.AvdeevAS.Sprint3.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+using Tyuiu.AvdeevAS.Sprint3.Task3.V1.Lib;
+namespace Tyuiu.AvdeevAS.Sprint3.Task3.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var varForTest = 2;
+            Assert.AreEqual(varForTest, ds.GetCharCount("have a nice time", 'a'));
+        }
+    }
+}
---
+++ Tyuiu.AvdeevAS.Sprint3.Task3.V1/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.AvdeevAS.Sprint3.Task3.V1.Lib;
+namespace Tyuiu.AvdeevAS.Sprint3.Task3.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Авдеев А.С. | ИБКСБ-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                  Спринт #3                              *");
+            Console.WriteLine("* Тема: <...>                                                             *");
+            Console.WriteLine("*                              Задание #2                                 *");
+            Console.WriteLine("*                              Вариант #18                                *");
+            Console.WriteLine("* Выполнил: Авдеев Андрей Сергеевич    | ИБКСБ-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                  УСЛОВИЕ:                               *");
+            Console.WriteLine("*      Написать программу, которая отвечает всем требованиям задания.     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                               ИСХОДНЫЕ ДАННЫЕ:                          *");
+            Console.WriteLine("***************************************************************************");
+
+            
+
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("РЕЗУЛЬТАТ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.GetCharCount("dfsasdfasdf", 'a' ));
+
+            Console.ReadKey();
+        }
+    }
+}
