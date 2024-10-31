aaa4d33f13fa8b708369165c495bab01fbd477e5
Tyuiu.AvdeevAS <Tyuiu.AvdeevAS@outlook.com>
2024-10-31 22:31:24 +0500

    d

---
+++ Tyuiu.AvdeevAS.Sprint3.Task4.V6.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.AvdeevAS.Sprint3.Task4.V6.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.AvdeevAS.Sprint3.Task4.V6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.AvdeevAS.Sprint3.Task4.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
